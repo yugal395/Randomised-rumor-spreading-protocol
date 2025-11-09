839cf7e9bee2b9698def4991826ed13adc9b1d33
Yugal Verma <yugalverma414@gmail.com>
2025-11-09 18:20:16 +0100

    Initial simulation push

---
+++ main.py
@@ -0,0 +1,65 @@
+import random  #importing the random module
+import matplotlib.pyplot as plt #importing the matplotlib.pyplot module for plotting
+
+
+# now we will create a function to simulate
+def simulate_push(n):
+#1: only one person knows the rumour initially
+    informed={0}   # we are labelling different informed people with number starting from 0 to n-1
+    rounds=0
+
+    #2: continue until everyone knows the rumour
+    while(len(informed)<n):
+       new_informed=set(informed)# here we are copying the people who already know the rumour and putting it in a new set as we dont want repetition of people
+       for u in informed:
+           v=random.randint(0,n-1) # so loop over the informed people and selects a random person v from the population
+           new_informed.add(v) # add v to the new informed set
+       informed=new_informed # update the informed set to the new informed set
+       rounds+=1 # increment the round count
+       
+    return rounds # return the total number of rounds taken to inform everyone
+   # so till here we have created a function to simulate the push model of rumour spreading
+
+
+print(simulate_push(10)) # testing the function with n=10 
+
+
+def simulate_pull(n):
+    informed={0}
+    rounds =0
+
+    #2. continue until everyone knows the rumour
+    while(len(informed)<n):
+        new_informed=set(informed)
+        for u in range(n):
+         v=random.randint(0,n-1)
+         if v in informed:
+                new_informed.add(u)
+        informed=new_informed
+        rounds+=1
+    return rounds     
+
+
+print(simulate_pull(10)) # testing the function with n=10
+
+
+def simulate_push_pull(n):
+    informed={0}
+    rounds=0
+
+    while(len(informed)<n):
+        new_informed=set(informed)
+        for u in range(n):
+            v=random.randint(0,n-1)
+            if u in informed:
+                new_informed.add(v)
+            elif v in informed:#if anybody is informed the other gets informed as well
+                new_informed.add(u)
+        informed=new_informed
+        rounds+=1        
+
+    return rounds
+
+
+print(simulate_push_pull(10)) # testing the function with n=10
+
